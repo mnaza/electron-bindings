88986801fe5ddcc496b47f65453daa1c77a63ac5
Ravi Shrestha <ravi.shrestha@sussol.net>
2021-05-20 02:07:48 +0545

    Package for binding native module in an electron app

---
+++ bindings.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import { existsSync } from 'fs';
+import { resolve } from 'path';
+import electron from 'electron';
+import nodeBindings from 'node-gyp-build';
+
+/**
+ * Binds the native module using node-gyp for the electron
+ * app.
+ * @param {*} nativePath Path to the native module.
+ * @returns Node binding instance for an electron app.
+ */
+const bindings = nativePath => {
+  let nativeModulePath = getLibPath(nativePath);
+  return nodeBindings(nativeModulePath);
+};
+
+const getLibPath = nativePath => {
+  if (typeof electron === 'string') {
+    throw new Error('Not running in an Electron environment!');
+  }
+  const { app } = electron;
+  const nodeModulesDir = resolve(app.isPackaged ? app.getAppPath() : process.cwd(), 'node_modules');
+  const libPath = resolve(nodeModulesDir, nativePath);
+  if (existsSync(nodeModulesDir)) {
+    if (existsSync(libPath)) {
+      return libPath;
+    }
+    return new Error('Could not find module: '+nativePath);
+  }
+  return new Error('No node modules have been installed!');
+};
+
+module.exports = bindings;
